d986b4dd9938499390705d961890b97e2e9a68ee
csuonereal <68502410+csuonereal@users.noreply.github.com>
2021-10-12 08:52:55 +0300

    google play config added, comparison bug fixed

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def set_get_driver(headless=False, strDriverPath="drivers/firefox.exe"):
     return driver
 
 
-def load_config_file(path="config.json"):
+def load_config_file(path="_google_play_/config.json"):
     if os.path.exists(path):
         with open(path, "r") as f:
             return json.load(f)
@@ -24,18 +24,28 @@ def load_config_file(path="config.json"):
 
 def run():
     data = load_config_file()
-
     urls = []
-    for item in data["countries"]:
-        urls.append([data["url"][:len("https://apps.apple.com/")] + item["code"] +
-                    "/"+data["url"][len("https://apps.apple.com/"):], item["code"]])
+    if data["platform"] == "app_store":
+        for item in data["countries"]:
+            urls.append([data["url"][:len("https://apps.apple.com/")] + item["code"] +
+                        "/"+data["url"][len("https://apps.apple.com/"):], item["code"]])
 
-    for url, code in urls:
-        driver = set_get_driver(
-            headless=True, strDriverPath=data["driver_path"])
-        scraper = ds("config.json", driver, url, code)
-        scraper.run()
-        time.sleep(0.2)
+        for url, code in urls:
+            driver = set_get_driver(
+                headless=True, strDriverPath=data["driver_path"])
+            scraper = ds("_app_store_/config.json", driver, url, code)
+            scraper.run()
+            time.sleep(0.2)
+    else:
+        for item in data["countries"]:
+            urls.append([data["url"]+f"&gl={item['code']}", item["code"]])
+
+        for url, code in urls:
+            driver = set_get_driver(
+                headless=True, strDriverPath=data["driver_path"])
+            scraper = ds("_google_play_/config.json", driver, url, code)
+            scraper.run()
+            time.sleep(0.2)
 
 
 run()
